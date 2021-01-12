b1dbfe7375b2f4da7dfc7ceda6b1ffc964b15ace
kakamesyli <58930490+kakamesyli@users.noreply.github.com>
2021-01-12 23:34:36 +0800

    Add files via upload
    202101122334

--- NSGAII.py
+++ NSGAII.py
@@ -12,14 +12,18 @@ import random
 import operator
 class Pop(object):
     def __init__(self,pop_index,x_min,x_max,x_num,f_num,fun_name):
-        self.index = pop_index; #¸öÌå±àºÅ
-        self.var = []; #¸öÌå±äÁ¿Öµ
+        self.index = pop_index; #ä¸ªä½“ç¼–å·
+        self.var = []; #ä¸ªä½“å˜é‡å€¼
         for i in range(x_num):
             self.cal_var(x_min,x_max);
-        self.output = []; #¸öÌåÊä³öÖµ
+        self.value = []; #ä¸ªä½“è¾“å‡ºå€¼
         for j in range(f_num):
             self.cal_output(self.var,x_num,fun_name[j]);
-        
+        self.pareto_rank = 0;
+        self.crowding = 0;
+        self.dom_num = 0;
+        self.dom_asm = [];
+
     def cal_var(self,x_min,x_max):
         self.var.append(x_min + (x_max-x_min) * random.random());
     
@@ -31,51 +35,61 @@ class Pop(object):
         
 class Chromo(object):
     def __init__(self,pop_num,x_min,x_max,x_num,f_num,fun_name):
+        self.pop_num = pop_num;
         self.pop_asm = [];
-        self.pareto_rank = 0;
-        self.crowding = 0;
-        self.dom_num = 0;
-        self.dom_asm = [];
         self.rank_asm = []; 
-        for i in range(pop_num):
-            self.create_pop_asm(self,pop_num,x_min,x_max,x_num,f_num,fun_name);
+        
+        '''
+        å®šä¹‰ä¸ªä½“é›†åˆ
+        '''
+        for i in range(self.pop_num):
+            self.create_pop_asm(self,i,x_min,x_max,x_num,f_num,fun_name);
     
     def create_pop_asm(self,pop_index,x_min,x_max,x_num,f_num,fun_name):
-            self.pop.append(Pop(pop_index,x_min,x_max,x_num,f_num,fun_name));
+            self.pop_asm.append(Pop(pop_index,x_min,x_max,x_num,f_num,fun_name));
     
         
-	def non_dominate_sort(self,pop_num,f_num):
-        for i in range(pop_num):
-            Chromo.dom_num = 0;
-            Chromo.dom_asm = [];
-            for j in pop_num:
+    def non_dominate_sort(self,pop_asm,f_num):
+        pareto_rank = 1;
+        for pop_i in pop_asm:
+            for pop_j in pop_asm:
                 less = 0;
                 equal = 0;
                 greater = 0;
-                if (i.value[0] < j.value[0] and i.value[1] < j.value[1]):
-                    less = less + 1;
-                elif (i.value[0] == j.value[0] and i.value[1] == j.value[1]):
-                    equal = equal + 1;
-                else:
-                    greater = greater + 1;
+                for k in range(f_num):
+                    if (pop_i.value[k] < pop_j.value[k]):
+                        less = less + 1;
+                    elif (pop_i.value[k] == pop_j.value[k]):
+                        equal = equal + 1;
+                    else:
+                        greater = greater + 1;
                 if (less == 0 and equal != f_num):
-                    Chromo.dom_num = Chromo.dom_num +1;
+                    pop_i.dom_num = pop_i.dom_num +1;
                 elif (greater == 0 and equal != f_num):
-                    Chromo.dom_asm.append(j);
-                if Chromo.dom_num == 0:
-                    Chromo.pareto_rank = 1;
-                    Chromo.rank_asm.append(i);
-        while (len(Chromo.rank_asm) != 0):
-            for pop in Chromo.rank_asm:
-                if (len(Chromo.dom_asm) != 0):
-
+                    pop_i.dom_asm.append(pop_j);
+                if pop_i.dom_num == 0:
+                    pop_i.pareto_rank = pareto_rank;
+                    self.rank_asm.append((pop_i.pareto_rank,pop_i));
+        while (len(self.rank_asm) != 0):
+            for rank_ele in self.rank_asm:
+                if (len(rank_ele.dom_asm) != 0):
+                    for dom_asm_ele in rank_ele.dom_asm:
+                        dom_asm_ele.dom_num = dom_asm_ele.dom_num - 1;
+                        if dom_asm_ele.dom_num == 0:
+                            dom_asm_ele.pareto_rank = pareto_rank + 1;
+                            self.rank_asm.append((dom_asm_ele.pareto_rank,dom_asm_ele));
+            pareto_rank = pareto_rank + 1;
+    def crowding_distance_sort(self):
+        temp = self.pop_asm.sort(cmp=None, key=None, reverse=False);
+        current_dist = 0;
+        
 def obj_fun(x,x_num,fun_name):
-        if operator.eq(fun_name, 'ZDT1'):
-            f = [];
-            f[0] = x[1];
-            s = 0;
-            for i in range(x):
-                s = s + x[i];
-            g = 1 + 9 * (s/(x_num-1));
-            f[1] = g * (1-(f[1]/g)^0.5);
+    if operator.eq(fun_name, 'ZDT1'):
+        f = [];
+        f[0] = x[1];
+        s = 0;
+        for i in range(x):
+            s = s + x[i];
+            g = 1 + 9 * (s / (x_num - 1));
+            f[1] = g * (1 - (f[1] / g) ^ 0.5);
         return f;

fc8127deb542467c233e2381e5450517b431a2b6
kakamesyli <58930490+kakamesyli@users.noreply.github.com>
2021-01-22 21:59:33 +0800

    20210122

--- NSGAII.py
+++ NSGAII.py
@@ -3,6 +3,9 @@
 Created on 202101011
 @author: think
 '''
+#from numpy.lib.user_array import temp
+#from numpy.random.mtrand import pareto
+#from _ctypes_test import func
 
 '''
 Created on 20210111
@@ -10,46 +13,46 @@ Created on 20210111
 '''
 import random
 import operator
+
+
 class Pop(object):
-    def __init__(self,pop_index,x_min,x_max,x_num,f_num,fun_name):
-        self.index = pop_index; #个体编号
-        self.var = []; #个体变量值
+
+    def __init__(self, pop_index, x_min, x_max, x_num, f_num, fun_name):
+        self.index = pop_index;  # #code
+        self.var = [];  #
         for i in range(x_num):
-            self.cal_var(x_min,x_max);
-        self.value = []; #个体输出值
+            self.cal_var(x_min, x_max);
+        self.value = [];  #
         for j in range(f_num):
-            self.cal_output(self.var,x_num,fun_name[j]);
+            self.cal_output(self.var, x_num, fun_name[j]);
         self.pareto_rank = 0;
         self.crowding = 0;
         self.dom_num = 0;
         self.dom_asm = [];
 
-    def cal_var(self,x_min,x_max):
-        self.var.append(x_min + (x_max-x_min) * random.random());
+    def cal_var(self, x_min, x_max):
+        self.var.append(x_min + (x_max - x_min) * random.random());
     
-    def cal_output(self,x,x_num,fun_name):
-        self.output.append(obj_fun(x,x_num,fun_name));
+    def cal_output(self, x, x_num, fun_name):
+        self.output.append(obj_fun(x, x_num, fun_name));
     
-    def create_rank_asm(self,Pop):
+    def create_rank_asm(self, Pop):
         self.rank_asm.append(Pop);
+
         
 class Chromo(object):
-    def __init__(self,pop_num,x_min,x_max,x_num,f_num,fun_name):
+
+    def __init__(self, pop_num, x_min, x_max, x_num, f_num, fun_name):
         self.pop_num = pop_num;
         self.pop_asm = [];
         self.rank_asm = []; 
-        
-        '''
-        定义个体集合
-        '''
         for i in range(self.pop_num):
-            self.create_pop_asm(self,i,x_min,x_max,x_num,f_num,fun_name);
+            self.create_pop_asm(self, i, x_min, x_max, x_num, f_num, fun_name);
     
-    def create_pop_asm(self,pop_index,x_min,x_max,x_num,f_num,fun_name):
-            self.pop_asm.append(Pop(pop_index,x_min,x_max,x_num,f_num,fun_name));
-    
+    def create_pop_asm(self, pop_index, x_min, x_max, x_num, f_num, fun_name):
+            self.pop_asm.append(Pop(pop_index, x_min, x_max, x_num, f_num, fun_name));
         
-    def non_dominate_sort(self,pop_asm,f_num):
+    def non_dominate_sort(self, pop_asm, f_num):
         pareto_rank = 1;
         for pop_i in pop_asm:
             for pop_j in pop_asm:
@@ -64,26 +67,96 @@ class Chromo(object):
                     else:
                         greater = greater + 1;
                 if (less == 0 and equal != f_num):
-                    pop_i.dom_num = pop_i.dom_num +1;
+                    pop_i.dom_num = pop_i.dom_num + 1;
                 elif (greater == 0 and equal != f_num):
                     pop_i.dom_asm.append(pop_j);
                 if pop_i.dom_num == 0:
                     pop_i.pareto_rank = pareto_rank;
-                    self.rank_asm.append((pop_i.pareto_rank,pop_i));
-        while (len(self.rank_asm) != 0):
-            for rank_ele in self.rank_asm:
+                    self.rank_asm[pareto_rank].append(pop_i);
+        while (len(self.rank_asm[pareto_rank]) != 0):
+            for rank_ele in self.rank_asm[pareto_rank]:
                 if (len(rank_ele.dom_asm) != 0):
                     for dom_asm_ele in rank_ele.dom_asm:
                         dom_asm_ele.dom_num = dom_asm_ele.dom_num - 1;
                         if dom_asm_ele.dom_num == 0:
                             dom_asm_ele.pareto_rank = pareto_rank + 1;
-                            self.rank_asm.append((dom_asm_ele.pareto_rank,dom_asm_ele));
+                            self.rank_asm[pareto_rank + 1].append(dom_asm_ele);
             pareto_rank = pareto_rank + 1;
-    def crowding_distance_sort(self):
-        temp = self.pop_asm.sort(cmp=None, key=None, reverse=False);
-        current_dist = 0;
+            
+    def crowding_distance_sort(self, pop_asm, f_num):
+        # temp = pop_asm.sorted(key=lambda x:x.pareto_rank, reverse=False);
+        for pareto_rank in range(len(self.rank_asm)):
+            for func in range(f_num):
+                func_temp = []
+                func_temp[pareto_rank] = sorted(self.rank_asm[pareto_rank],key=lambda x:x.value[func], reverse=False)
+                current_index = 0;
+                fmin = func_temp[pareto_rank][0].value[func]
+                fmax = func_temp[pareto_rank][-1].value[func]
+                func_temp[pareto_rank][0] = float('Inf')
+                func_temp[pareto_rank][-1] = float('Inf')
+                for i in range(1,len(func_temp[pareto_rank])-2):#for pop in func_temp[pareto_rank][1, -2]:
+                    next_fun_value = func_temp[pareto_rank][i-1].value[func]#next_fun_value = func_temp[pareto_rank][pop.index()-1].value[func]
+                    pre_fun_value = func_temp[pareto_rank][i+1].value[func]#pre_fun_value = func_temp[pareto_rank][pop.index()+1].value[func]
+                    if fmax == fmin:
+                        func_temp[pareto_rank][i].crowding = float('Inf')
+                    else:
+                        func_temp[pareto_rank][i].crowding = func_temp[pareto_rank][i].crowding + (next_fun_value - pre_fun_value) / (fmax - fmin)
+            func_temp[pareto_rank] = sorted(func_temp[pareto_rank],key=lambda x:x.index, reverse=False)
+            for i in range(len(func_temp[pareto_rank])):
+                self.rank_asm[pareto_rank][i].crowding = func_temp[pareto_rank][i].crowding
+                
+                '''
+            for pop in temp[current_index, len(self.rank_asm[pareto_rank]) - 1]:
+                for func in range(f_num):
+                    fun_value_temp[func] = 
+                    fun_value_temp[func].append(pop.value[func])
+            
+                y_asm.append(pop)
+            for f in range(f_num):
+                f_sort[f] !! = y_asm.sorted(key=lambda x:x.value[f], reverse=False)
+                fmin = f_sort[1];
+                fmax = f_sort[-1]
+                f_sort[1].crowding = float('inf')
+                f_sort[-1].crowding = float('inf')
+                for f_s in range(2, len(f_sort) - 1):
+                    pre_f = f_sort[f_s - 1]
+                    next_f = f_sort[f_s + 1]
+                    if fmin == fmax:
+                        f_sort[f_s] = float('inf')
+                    else:
+                        f_sort[f_s] = (next_f - pre_f) / (fmax - fmin)
+            for f in range(f_num):
+                nd = 
+'''
+def elitsm(pop_num,chromo2):
+    chromo_temp = sorted(chromo2,key = lambda x:x.pareto_rank,reverse = False)
+    pre_ind = 0
+    current_rank = 1
+    current_ind = []
+    chromo_elit = []
+    for pop in chromo_temp:
+        if pop.pareto_rank == current_rank:
+            current_ind[current_rank] = current_ind[current_rank] + 1
+            
+        if pop.pareto_rank == current_rank:
+            if current_ind < pop_num:
+                chromo_elit.append(pop)
+                current_ind = current_ind + 1
+            elif current_ind > pop_num and current_ind == pop_num:
+                
+        elif pop.pareto_rank > current_rank:
+            
         
-def obj_fun(x,x_num,fun_name):
+            
+            chromo_temp = chromo_temp + 1
+        elif current_ind > pop_num and current_ind == pop_num:
+            
+    for i in range(1,max(chromo_temp.pareto_rank)):
+        current_index_pareto = 
+        
+    return chromo_elit
+            
+def obj_fun(x, x_num, fun_name):
     if operator.eq(fun_name, 'ZDT1'):
         f = [];
         f[0] = x[1];
@@ -93,3 +166,23 @@ def obj_fun(x,x_num,fun_name):
             g = 1 + 9 * (s / (x_num - 1));
             f[1] = g * (1 - (f[1] / g) ^ 0.5);
         return f;
+
+    
+class Person():
+
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+
+def personSort():
+    persons = [Person(age, name) for (age, name) in [(12, "lili"), (18, "lulu"), (16, "kaka"), (12, "xixi")]]
+    persons.sort(key=lambda x:x.name, reverse=False);
+    for element in persons:
+        print (element.age, ":", element.name);
+
+        
+if __name__ == "__main__":
+    pop = [Pop(pop_index, x_min, x_max, x_num, f_num, fun_name) for (pop_index, x_min, x_max, x_num, f_num, fun_name) in [(1, 1, 1, 1, 1, 'ZDT1'),(2, 1, 1, 1, 1, 'ZDT1')]]
+    for i in pop:
+        print(i.index())
